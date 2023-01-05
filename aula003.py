a99ef166a50c66f0daed5d04b9cbe00bae68c17e
Julio Gazoli <julio_gazoli@hotmail.com>
2023-01-05 17:48:57 -0300

    aula003

---
+++ aula003.py
@@ -0,0 +1,22 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+print('Julio Gazoli')
+print('Juliio "Gazoli"')
+
+# Aspas duplas
+print("Julio Gazoli")
+print("Julio 'Gazoli'")
+
+# Escape
+print("Julio \"Gazoli\"")
+
+# r
+print(r"Julio \"Gazoli\"")
