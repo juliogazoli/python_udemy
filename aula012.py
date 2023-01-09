4827d34480fc02965e54591f2c375a2f00f6284e
Julio Gazoli <julio_gazoli@hotmail.com>
2023-01-09 19:54:50 -0300

    aula012

---
+++ aula012.py
@@ -0,0 +1,11 @@
+nome = 'Julio Gazoli'
+altura = 1.75
+peso = 75
+imc = peso / (altura * altura)
+
+# Julio Gazoli tem 1.80 de altura,
+# pesa 75 quilos e seu IMC é
+# 24.489795918367346
+
+print(nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilos e seu IMC é', imc)
