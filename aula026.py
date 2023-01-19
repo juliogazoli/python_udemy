d6e17b25e545956571a8331c789dac6f02ed1470
Julio Gazoli <julio_gazoli@hotmail.com>
2023-01-19 15:46:59 -0300

    aula026

---
+++ aula026.py
@@ -0,0 +1,17 @@
+"""
+Formação básica de strings
+s - string
+d - int
+f - flaot
+.<número de dígitos>f
+x ou X - Hexadecimal
+(Caractere)(><^)(quantidade)
+> - Esquerda
+< - Direita
+^ - Centro
+Sinal - + ou -
+Ex.: 0>-100,1.f
+Conversion flags - !r !s !a
+"""
+
+
