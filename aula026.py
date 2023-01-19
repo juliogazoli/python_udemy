bfc98ada9739927eb1aca8fc6aec14ac4028e90d
Julio Gazoli <julio_gazoli@hotmail.com>
2023-01-19 15:58:09 -0300

    aula026

--- aula026.py
+++ aula026.py
@@ -2,16 +2,26 @@
 Formação básica de strings
 s - string
 d - int
-f - flaot
+f - float
 .<número de dígitos>f
 x ou X - Hexadecimal
 (Caractere)(><^)(quantidade)
 > - Esquerda
 < - Direita
 ^ - Centro
+= - Força o número a aparecer antes dos zeros
 Sinal - + ou -
 Ex.: 0>-100,1.f
 Conversion flags - !r !s !a
 """
 
-
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}.')
+print(f'{variavel: ^10}.')
+print(f'{variavel:#^10}.')
+print(f'{1000.4879645612301:+.1f}')
+print(f'{1000.4879645612301:0=+10,.1f}')
+print(f'O hexadecimal de 1500 é {1500:08X}')
+print(f'{variavel!r}')
