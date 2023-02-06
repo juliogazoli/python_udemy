18d754158bc5251f589e197d2ac2d064df1065dc
Julio Gazoli <julio_gazoli@hotmail.com>
2023-02-06 16:39:56 -0300

    aula031

--- aula031.py
+++ aula031.py
@@ -11,8 +11,15 @@ id = Identidade
 # print(id(v2))
 
 condicao = False
+passou_no_if = None
 
 if condicao:
-  print('Faça algo')
+    passou_no_if = True
+    print('Faça algo')
 else:
-  print('Não faça algo')
+    print('Não faça algo')
+
+if passou_no_if is None:
+    print('Não passou no if')
+else:
+    print('Passou no if')
