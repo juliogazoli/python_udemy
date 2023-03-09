f1513b50d5d28c506b0285ac33af79917cd1cd58
Julio Gazoli <julio_gazoli@hotmail.com>
2023-03-09 19:28:44 -0300

    Aula 052

---
+++ aula052.py
@@ -0,0 +1,13 @@
+"""
+Tipo tupla - Uma lista imutável
+"""
+# nomes_tupla = ('Maria', 'Helena', 'Luiz')
+nomes_tupla = 'Maria', 'Helena', 'Luiz'
+
+# nomes_tupla[1] = 'Outro' # Erro
+
+print(nomes_tupla)
+print(type(nomes_tupla))
+
+# lista = ['Maria', 'Helena', 'Luiz']
+# tupla = tuple(lista)
