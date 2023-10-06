7acedf8abd191fde4db996ff3de32f1706fff871
juliogazoli <julio_gazoli@hotmail.com>
2023-10-06 11:57:59 -0300

    Aula 062
    Solução do exercício - Gerar o segundo dígito de um CPF com Python

--- aula062.py
+++ aula062.py
@@ -24,3 +24,55 @@ contrário disso:
     
 O segundo dígito do CPF é 0
 """
+
+# # Minha solução
+# cpf = '746.824.890-70'
+# cpf_so_numeros = cpf.replace('.', '').replace('-', '')
+
+# produto = 0
+# soma = 0
+# posicao = 0
+# for multiplicador in range(11, 1, -1):
+#     produto = int(cpf_so_numeros[posicao]) * multiplicador
+#     soma += produto
+#     posicao += 1
+
+# multiplicar_por_dez = soma * 10
+
+# resto_da_divisao = multiplicar_por_dez % 11
+
+# segundo_digito = 0 if resto_da_divisao > 9 else resto_da_divisao
+
+# print(segundo_digito)
+
+
+# Solução:
+cpf_enviado_usuario = '74682489070'
+nove_digitos = cpf_enviado_usuario[:9]
+contador_regressivo_1 = 10
+
+resultado_digito_1 = 0
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -= 1
+
+digito_1 = ((resultado_digito_1 * 10) % 11)
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+
+resultado_digito_2 = 0
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+
+digito_2 = ((resultado_digito_2 * 10) % 11)
+digito_2 = digito_2 if digito_2 <= 9 else 0
+# print(digito_2)
+
+cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+# print(cpf_gerado_pelo_calculo)
+
+if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
+    print(f'{cpf_enviado_usuario} é válido')
