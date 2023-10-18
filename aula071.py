ea7d30082ada9ea91c4f724968a2f253fe746a96
juliogazoli <julio_gazoli@hotmail.com>
2023-10-18 12:50:14 -0300

    Aula 071
    (Parte 2) *args para quantidade de argumentos não nomeados variáveis

--- aula071.py
+++ aula071.py
@@ -13,10 +13,23 @@ args - Agumentos não nomeados
 def soma(*args):
     total = 0
     for numero in args:
-        print('Total', total, numero)
         total += numero
-        print('Total', total)
-    print(total)
+    return total
 
 
-soma(1, 2, 3, 4, 5, 6)
+soma_1_2_3 = soma(1, 2, 3)
+print(soma_1_2_3)
+
+soma_4_5_6 = soma(4, 5, 6)
+print(soma_4_5_6)
+
+# outra_soma = soma(1, 2, 3, 4, 5, 6, 7, 78, 10)
+# print(outra_soma)
+
+# print(sum((1, 2, 3, 4, 5, 6, 7, 78, 10)))
+
+numeros = 1, 2, 3, 4, 5, 6, 7, 78, 10
+outra_soma = soma(*numeros)
+print(outra_soma)
+
+print(sum(numeros))
