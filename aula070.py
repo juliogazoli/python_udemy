d6f243a5222b7e6f48f81a43b7794307e8c49c0a
juliogazoli <julio_gazoli@hotmail.com>
2023-10-18 12:18:41 -0300

    Aula 070
    Retorno de valores das funções (return)

---
+++ aula070.py
@@ -0,0 +1,45 @@
+"""
+Retorno de valores das funções (return)
+"""
+
+# variavel = print('Julio')
+# print(variavel)
+# print(variavel is None)
+
+
+# def soma(x, y):
+#     ...
+
+
+# variavel = soma(1, 2)
+# print(variavel)
+
+
+# variavel = int('1')
+# print(variavel)
+
+
+# def soma(x, y):
+#     print('Olha')
+#     print('só')
+#     print('que')
+#     print('legal')
+#     return x + y
+#     print(1 + 1)
+
+# soma1 = soma(2, 2)
+# soma2 = soma(3, 3)
+# print(soma1 + soma2)
+
+
+def soma(x, y):
+    if x > 10:
+        return [10, 20]
+    return x + y
+
+
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+print(soma1)
+print(soma2)
+print(soma(11, 55))
