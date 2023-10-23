321b5a63981a22dcf5aeda3d35b5eb99c70e8e8f
juliogazoli <julio_gazoli@hotmail.com>
2023-10-23 12:22:42 -0300

    Aula 075
    Exercício com funções

---
+++ aula075.py
@@ -0,0 +1,22 @@
+"""
+Exercícios
+Crie funções que duplicam, triplicam e quadruplicam
+o número recebido como parâmetro.
+"""
+
+# # Minha solução
+def duplica(numero):
+    return numero * 2
+
+
+def triplica(numero):
+    return numero * 3
+
+
+def quadruplica(numero):
+    return numero * 4
+
+
+print(duplica(2))
+print(duplica(3))
+print(duplica(4))
