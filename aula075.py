4387dccc4e6e21aef15c2d31f7cd5a986004dbfc
juliogazoli <julio_gazoli@hotmail.com>
2023-10-23 12:25:00 -0300

    Aula 075
    Solução do exercício com funções

--- aula075.py
+++ aula075.py
@@ -5,18 +5,33 @@ o número recebido como parâmetro.
 """
 
 # # Minha solução
-def duplica(numero):
-    return numero * 2
+# def duplica(numero):
+#     return numero * 2
 
 
-def triplica(numero):
-    return numero * 3
+# def triplica(numero):
+#     return numero * 3
 
 
-def quadruplica(numero):
-    return numero * 4
+# def quadruplica(numero):
+#     return numero * 4
 
+
+# print(duplica(2))
+# print(duplica(3))
+# print(duplica(4))
+
+
+# Solução
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+duplica = criar_multiplicador(2)
+triplica = criar_multiplicador(3)
+quadruplica = criar_multiplicador(4)
 
 print(duplica(2))
-print(duplica(3))
-print(duplica(4))
+print(triplica(2))
+print(quadruplica(2))
