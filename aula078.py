3baf76071dd3019cffe25b5b8f48f03eab228f5a
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-10-26 12:46:02 -0300

    Aula 078
    Peculiaridades do tipo mutável set em Python

--- aula078.py
+++ aula078.py
@@ -19,11 +19,29 @@ Métodos úteis:
 add, update, clear, discard
 """
 
-s1 = set()
-print(s1, type(s1))
+# s1 = set()
+# print(s1, type(s1))
 
-s1 = set('Julio')
-print(s1)
+# s1 = set('Julio')
+# print(s1)
 
-s1 = {'Julio', 1, 2, 3}
-print(s1)
+# s1 = {'Julio', 1, 2, 3}
+# print(s1)
+
+
+# s1 = {1, 2, 3, 3, 3, 3, 3, 1}
+# print(s1)
+
+# l1 = [1, 2, 3, 3, 3, 3, 3, 1]
+# s1 = set(l1)
+# l2 = list(s1)
+# print(l2)
+
+# s1 = {1, 2, 3, [123]} # Erro: não aceita lista
+# s1 = {1, 2, 3, {123}} # Erro: não aceita set
+# s1 = {1, 2, 3, {'chave': 'valor'}} # Erro: não aceita set
+# s1 = {1, 2, 3, (123,)} # Ok
+
+s1 = {1, 2, 3}
+for numero in s1:
+    print(numero)
