7f2e04c40a371171901cb97aa19eae5f449ae4b8
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-10-26 12:21:52 -0300

    Aula 077
    Solução do Exercício - sistema de perguntas e respostas com Python

--- aula077.py
+++ aula077.py
@@ -2,7 +2,44 @@
 Exercício - sistema de perguntas e respostas
 """
 
-# Minha solução
+# # Minha solução
+# perguntas = [
+#     {
+#         'Pergunta': 'Quanto é 2+2?',
+#         'Opções': ['1', '3', '4', '5'],
+#         'Resposta': '4',
+#     },
+#     {
+#         'Pergunta': 'Quanto é 5*5?',
+#         'Opções': ['25', '55', '10', '51'],
+#         'Resposta': '25',
+#     },
+#     {
+#         'Pergunta': 'Quanto é 10/2?',
+#         'Opções': ['4', '5', '2', '1'],
+#         'Resposta': '5',
+#     },
+# ]
+
+# acertos = 0
+# contador = 0
+# for pergunta in perguntas:
+#     print(f'Pergunta: {pergunta["Pergunta"]}\n')
+#     print('Opções:')
+#     for indice, opcao in enumerate(pergunta['Opções']):
+#         print(f'{indice}) {opcao}')
+#     resposta_usuario = int(input('Escolha uma opção: '))
+#     resposta_correta = pergunta['Opções'].index(pergunta['Resposta'])
+#     if resposta_correta == resposta_usuario:
+#         print('Acertou\n')
+#         acertos += 1
+#     else:
+#         print('Errou\n')
+#     contador += 1
+# print(f'Você acertou {acertos} de {contador} perguntas.')
+
+
+# Solução
 perguntas = [
     {
         'Pergunta': 'Quanto é 2+2?',
@@ -21,19 +58,38 @@ perguntas = [
     },
 ]
 
-acertos = 0
-contador = 0
+qtd_acertos = 0
 for pergunta in perguntas:
-    print(f'Pergunta: {pergunta["Pergunta"]}\n')
-    print('Opções:')
-    for indice, opcao in enumerate(pergunta['Opções']):
-        print(f'{indice}) {opcao}')
-    resposta_usuario = int(input('Escolha uma opção: '))
-    resposta_correta = pergunta['Opções'].index(pergunta['Resposta'])
-    if resposta_correta == resposta_usuario:
-        print('Acertou\n')
-        acertos += 1
+    print('Pergunta:', pergunta['Pergunta'])
+    print()
+    
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(opcoes):
+        print(f'{i})', opcao)
+    print()
+    
+    escolha = input('Escolha uma opção: ')
+    
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+    
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+        
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
+    
+    print()
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou 👍')
     else:
-        print('Errou\n')
-    contador += 1
-print(f'Você acertou {acertos} de {contador} perguntas.')
+        print('Errou ❌ ')
+        
+    print()
+
+print('Você acertou', qtd_acertos)
+print('de', len(perguntas), 'perguntas.')
