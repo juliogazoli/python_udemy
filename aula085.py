e38bfaed179436c34d52a8855dcbd93b1145889b
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-11-08 12:22:52 -0300

    Aula 085
    List comprehension com mais de um for

---
+++ aula085.py
@@ -0,0 +1,23 @@
+# lista = []
+# for x in range(3):
+#     for y in range(3):
+#         lista.append((x, y))
+
+# print(lista)
+
+
+# lista = [
+#     (x, y)
+#     for x in range(3)
+#     for y in range(3)
+# ]
+
+# print(lista)
+
+
+lista = [
+    [(x, letra) for letra in 'Luiz']
+    for x in range(3)
+]
+
+print(lista)
