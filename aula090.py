53ac0df297a71c9d107d1dca80e9d6caeb4728d9
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-11-13 10:52:12 -0300

    Aula 090
    Mais detalhes sobre Iterables e Iterators (Iteráveis e Iteradores)

---
+++ aula090.py
@@ -0,0 +1,12 @@
+"""
+Generator expression, Iterables e Iterators em Python
+"""
+iterable = ['Eu', 'Tenho', '__iter__']
+
+# iterator = iterable.__iter__() # tem __iter__ e __next__
+iterator = iter(iterable)
+
+print(iterator)
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
