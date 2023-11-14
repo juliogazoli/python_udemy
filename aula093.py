9d661531ae191f529743bc47d94f213fc3f5bb13
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-11-14 09:45:23 -0300

    Aula 093
    (Parte 2) try e except para tratar exceções

--- aula093.py
+++ aula093.py
@@ -4,16 +4,20 @@ Try, except, else e finally
 try:
     a = 18
     b = 0
-    print(b[0])
-    print('Linha 1'[1000])
+    # print(b[0])
+    # print('Linha 1'[1000])
     c = a / b
     print('Linha 2')
-except ZeroDivisionError:
+except ZeroDivisionError as e:
+    print(e)
+    print(e.__class__.__name__)
     print('Dividiu por zero.')
 except NameError:
     print('Nome b não está definido.')
-except (TypeError, IndexError):
+except (TypeError, IndexError) as error:
     print('TypeError + IndexError')
+    print('MSG: ', error)
+    print('Nome: ', error.__class__.__name__)
 except Exception:
     print('ERRO DESCONHECIDO.')
 
