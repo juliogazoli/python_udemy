55e06ba7c39f152c776c675263ee52c5d26e90af
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-11-16 10:15:46 -0300

    Aula 099
    O ponto de vista do __main__ pode te confundir em módulos e pacotes Python

--- aula099.py
+++ aula099.py
@@ -1,13 +1,18 @@
-from sys import path
+# from sys import path
 
-import aula099_package.modulo
-from aula099_package import modulo
-from aula099_package.modulo import soma_do_modulo
-from aula099_package.modulo import *
+# import aula099_package.modulo
+# from aula099_package import modulo
+# from aula099_package.modulo import soma_do_modulo
+# from aula099_package.modulo import *
 
-# print(*path, sep='\n')
-print(aula099_package.modulo.soma_do_modulo(1, 2))
-print(modulo.soma_do_modulo(1, 2))
-print(soma_do_modulo(1, 2))
-print(variavel)
-# print(nova_variavel)
+# # print(*path, sep='\n')
+# print(aula099_package.modulo.soma_do_modulo(1, 2))
+# print(modulo.soma_do_modulo(1, 2))
+# print(soma_do_modulo(1, 2))
+# print(variavel)
+# # print(nova_variavel)
+
+
+from aula099_package.modulo import soma_do_modulo, fala_oi
+print(__name__)
+fala_oi()
--- aula099_package/modulo.py
+++ aula099_package/modulo.py
@@ -1,7 +1,19 @@
-__all__ = [
-    'variavel',
-    'soma_do_modulo',
-]
+# __all__ = [
+#     'variavel',
+#     'soma_do_modulo',
+# ]
+
+# variavel = 'Alguma coisa'
+
+
+# def soma_do_modulo(x, y):
+#     return x + y
+
+
+# # nova_variavel = 'OK'
+
+
+from aula099_package.modulo_b import fala_oi
 
 variavel = 'Alguma coisa'
 
@@ -10,4 +22,4 @@ def soma_do_modulo(x, y):
     return x + y
 
 
-# nova_variavel = 'OK'
+# fala_oi()
