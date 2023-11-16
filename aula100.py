b902de0c11f85be18ff8eeeb4cde0053a703bde1
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-11-16 12:04:00 -0300

    Aula 100
    Proposta de 3 exercícios em um

---
+++ aula100.py
@@ -0,0 +1,23 @@
+"""
+Exercícios
+Aumente os preços dos produtos a seguir em 10%
+Gere novos_produtos por deep copy (cópia profunda)
+"""
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+"""
+Ordene os produtos por nome decrescente (do maior para o menor)
+Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+"""
+
+"""
+Ordene os produtos por preço crescente (do menor para o maior)
+Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+"""
