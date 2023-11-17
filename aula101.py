61c41eef58979f59447c56afd3d2c10f6d8e77cf
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-11-17 08:31:21 -0300

    Aula 101
    Solução - Exercício - Adiando execução de funções

--- aula101.py
+++ aula101.py
@@ -11,9 +11,14 @@ def multiplica(x, y):
     return x * y
 
 
-def executa(funcao, *args):
-    return funcao(*args)
+def criar_funcao(funcao, x):
+    def interna(y):
+        return funcao(x, y)
+    return interna
 
 
-soma_com_cinco = executa(soma, 5)
-multiplica_por_dez = executa(multiplica, 10)
+soma_com_cinco = criar_funcao(soma, 5)
+multiplica_por_dez = criar_funcao(multiplica, 10)
+
+print(soma_com_cinco(10))
+print(multiplica_por_dez(10))
