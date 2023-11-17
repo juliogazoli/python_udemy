57f77ad97df7f7c030325901a61cd146abe6b0ae
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-11-17 12:25:40 -0300

    Aula 107_2
    Exercícios - somando duas listas

---
+++ aula107_2.py
@@ -0,0 +1,57 @@
+"""
+Considerando duas listas de inteiros ou floats (lista A e lista B)
+Some os valores nas listas retornando uma nova lista com os valores somados:
+
+Se uma lista for maior que a outra, a soma só vai considerar o tamanho da
+menor.
+
+Exemplo:
+lista_a     = [1, 2, 3, 4, 5, 6, 7]
+lista_b     = [1, 2, 3, 4]
+
+=================== resultado
+lista_soma  = [2, 4, 6, 8]
+"""
+
+# # Minha Solução
+# lista_a = [1, 2, 3, 4, 5, 6, 7]
+# lista_b = [1, 2, 3, 4]
+
+
+# def zipper_soma(l1, l2):
+#     limite = min(len(l1), len(l2))
+#     lista_soma = [l1[i] + l2[i] for i in range(limite)]
+#     return lista_soma
+
+
+# print(zipper_soma(lista_a, lista_b))
+
+
+# # Solução 1
+# lista_a = [1, 2, 3, 4, 5, 6, 7]
+# lista_b = [1, 2, 3, 4]
+
+# lista_soma = []
+# for i in range(len(lista_b)):
+#     lista_soma.append(lista_a[i] + lista_b[i])
+
+# print(lista_soma)
+
+
+# # Solução 2
+# lista_a = [1, 2, 3, 4, 5, 6, 7]
+# lista_b = [1, 2, 3, 4]
+
+# lista_soma = []
+# for i, _ in enumerate(lista_b):
+#     lista_soma.append(lista_a[i] + lista_b[i])
+
+# print(lista_soma)
+
+
+# Solução 3
+lista_a = [1, 2, 3, 4, 5, 6, 7]
+lista_b = [1, 2, 3, 4]
+
+lista_soma = [x + y for x, y in zip(lista_a, lista_b)]
+print(lista_soma)
