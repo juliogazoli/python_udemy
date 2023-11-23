389991da09397465fa78f4f085cc76388e016058
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-11-23 09:58:57 -0300

    Aula 116
    with open (context manager) e métodos úteis do TextIOWrapper

--- aula116.py
+++ aula116.py
@@ -32,6 +32,50 @@ caminho_arquivo = 'aula116.txt'
 # #
 # arquivo.close()
 
-with open(caminho_arquivo, 'w') as arquivo:
-    print('Olá mundo')
-    print('Arquivo vai ser fechado')
+# with open(caminho_arquivo, 'w') as arquivo:
+#     print('Olá mundo')
+#     print('Arquivo vai ser fechado')
+
+# with open(caminho_arquivo, 'w') as arquivo:
+#     arquivo.write('Linha 1\n')  # Windows \r\n
+#     arquivo.write('Linha 2\n')
+
+# with open(caminho_arquivo, 'r') as arquivo:
+#     print(arquivo.read())
+
+
+# with open(caminho_arquivo, 'w+') as arquivo:
+#     arquivo.write('Linha 1\n')  # Windows \r\n
+#     arquivo.write('Linha 2\n')
+#     arquivo.seek(0, 0)
+#     print(arquivo.read())
+
+
+# with open(caminho_arquivo, 'w+') as arquivo:
+#     arquivo.write('Linha 1\n')  # Windows \r\n
+#     arquivo.write('Linha 2\n')
+#     arquivo.writelines(('Linha 3\n', 'Linha 4\n'))
+#     arquivo.seek(0, 0)
+#     print(arquivo.read())
+
+
+# with open(caminho_arquivo, 'w+') as arquivo:
+#     arquivo.write('Linha 1\n')  # Windows \r\n
+#     arquivo.write('Linha 2\n')
+#     arquivo.writelines(('Linha 3\n', 'Linha 4\n'))
+#     print('Lendo')
+#     arquivo.seek(0, 0)
+#     print(arquivo.readline(), end='')
+#     print(arquivo.readline().strip())
+#     print(arquivo.readline())
+#     print(arquivo.readline())
+
+
+with open(caminho_arquivo, 'w+') as arquivo:
+    arquivo.write('Linha 1\n')  # Windows \r\n
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(('Linha 3\n', 'Linha 4\n'))
+    print('ReadLines')
+    arquivo.seek(0, 0)
+    for linha in arquivo:
+        print(linha.strip())
