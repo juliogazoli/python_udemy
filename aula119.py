f0e572a2d2bba61c3132631f677442f98d593703
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-11-24 10:26:09 -0300

    Aula 119
    Exercício (+solução) - salvando a lista de tarefas em JSON

--- aula119.py
+++ aula119.py
@@ -60,6 +60,7 @@ refazer = todo ['fazer café', 'caminhar']
 
 # Solução:
 import os
+import json
 
 
 def listar(tarefas):
@@ -109,8 +110,27 @@ def adicionar(tarefa, tarefas):
     listar(tarefas)
 
 
-tarefas = []
+def ler(tarefas, caminho_arquivo):
+    dados = []
+    try:
+        with open(caminho_arquivo, 'r', encoding='utf8') as arquivo:
+            dados = json.load(arquivo)
+    except FileNotFoundError:
+        print('Arquivo não existe')
+        salvar(tarefas, caminho_arquivo)
+    return dados
+
+
+def salvar(tarefas, caminho_arquivo):
+    with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+        dados = json.dump(tarefas, arquivo, indent=2, ensure_ascii=False)
+
+
+CAMINHO_ARQUIVO = 'aula119.json'
+tarefas = ler([], CAMINHO_ARQUIVO)
 tarefas_refazer = []
+
+caminho = 'aula119.json'
 
 while True:
     print('Comandos: listar, desfazer e refazer')
@@ -126,6 +146,7 @@ while True:
     comando = comandos.get(tarefa) if comandos.get(
         tarefa) is not None else comandos['adicionar']
     comando()
+    salvar(tarefas, CAMINHO_ARQUIVO)
 
     # if tarefa == 'listar':
     #     listar(tarefas)
