46aac5c1da8c4d31c5eb5e666336aae2febd6052
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-11-24 08:46:19 -0300

    Aula 119
    Minha Solução

--- aula119.py
+++ aula119.py
@@ -10,3 +10,49 @@ desfazer = [] -> Refazer ['caminhar', 'fazer café']
 refazer = todo ['fazer café']
 refazer = todo ['fazer café', 'caminhar']
 """
+
+# Minha Solução:
+import os
+
+todo = []
+refazer = []
+
+
+def imprime_tarefas():
+    print()
+    print('TAREFAS:')
+    for tarefa in todo:
+        print(tarefa)
+    print()
+
+
+while True:
+    print('Comandos: listar, desfazer, refazer')
+    resposta = input('Digite uma tarefa ou comando: ')
+    resposta_formatada = resposta.lower().strip()
+
+    if resposta_formatada == 'listar':
+        imprime_tarefas()
+
+    elif resposta_formatada == 'desfazer':
+        if not todo:
+            print()
+            print('Nada a desfazer')
+        else:
+            refazer.append(todo.pop())
+        imprime_tarefas()
+
+    elif resposta_formatada == 'refazer':
+        if not refazer:
+            print()
+            print('Nada a refazer')
+        else:
+            todo.append(refazer.pop())
+        imprime_tarefas()
+
+    elif resposta_formatada == 'clear':
+        os.system('clear')
+
+    else:
+        todo.append(resposta_formatada)
+        imprime_tarefas()
