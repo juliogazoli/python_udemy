b7693bfb47ae8bb36a24b59588987ce0503bfdf0
Julio Gazoli <julio.gazoli@sorocaba.sp.gov.br>
2023-11-27 12:38:44 -0300

    Aula 122
    Escopo da classe e de métodos da classe

---
+++ aula122.py
@@ -0,0 +1,25 @@
+"""
+Entendendo self em classes Python
+Classe - Molde (geralmente sem dados)
+Instância da classe (objeto) - Tem os dados
+Uma classe pode gerar várias instâncias.
+Na classe o self é a própria instância.
+"""
+
+
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando')
+
+
+fusca = Carro('Fusca')
+Carro.acelerar(fusca)
+# print(fusca.nome)
+# fusca.acelerar()
+
+celta = Carro(nome='Celta')
+# print(celta.nome)
+# celta.acelerar()
